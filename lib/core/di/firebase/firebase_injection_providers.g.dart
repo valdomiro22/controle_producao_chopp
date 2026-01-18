942f87bebe23baa0c0f83f23384085f741ffbcca
Valdomiro Santos <valdomiro.jose@outlook.com>
2026-01-18 18:52:28 -0300

    deletar producao

---
+++ lib/core/di/firebase/firebase_injection_providers.g.dart
@@ -0,0 +1,367 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'firebase_injection_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(firebaseAuth)
+final firebaseAuthProvider = FirebaseAuthProvider._();
+
+final class FirebaseAuthProvider
+    extends $FunctionalProvider<FirebaseAuth, FirebaseAuth, FirebaseAuth>
+    with $Provider<FirebaseAuth> {
+  FirebaseAuthProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'firebaseAuthProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$firebaseAuthHash();
+
+  @$internal
+  @override
+  $ProviderElement<FirebaseAuth> $createElement($ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  FirebaseAuth create(Ref ref) {
+    return firebaseAuth(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(FirebaseAuth value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<FirebaseAuth>(value),
+    );
+  }
+}
+
+String _$firebaseAuthHash() => r'912368c3df3f72e4295bf7a8cda93b9c5749d923';
+
+@ProviderFor(firebaseFirestore)
+final firebaseFirestoreProvider = FirebaseFirestoreProvider._();
+
+final class FirebaseFirestoreProvider
+    extends
+        $FunctionalProvider<
+          FirebaseFirestore,
+          FirebaseFirestore,
+          FirebaseFirestore
+        >
+    with $Provider<FirebaseFirestore> {
+  FirebaseFirestoreProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'firebaseFirestoreProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$firebaseFirestoreHash();
+
+  @$internal
+  @override
+  $ProviderElement<FirebaseFirestore> $createElement(
+    $ProviderPointer pointer,
+  ) => $ProviderElement(pointer);
+
+  @override
+  FirebaseFirestore create(Ref ref) {
+    return firebaseFirestore(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(FirebaseFirestore value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<FirebaseFirestore>(value),
+    );
+  }
+}
+
+String _$firebaseFirestoreHash() => r'963402713bf9b7cc1fb259d619d9b0184d4dcec1';
+
+@ProviderFor(authRemoteDatasource)
+final authRemoteDatasourceProvider = AuthRemoteDatasourceProvider._();
+
+final class AuthRemoteDatasourceProvider
+    extends
+        $FunctionalProvider<
+          AuthRemoteDatasource,
+          AuthRemoteDatasource,
+          AuthRemoteDatasource
+        >
+    with $Provider<AuthRemoteDatasource> {
+  AuthRemoteDatasourceProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'authRemoteDatasourceProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$authRemoteDatasourceHash();
+
+  @$internal
+  @override
+  $ProviderElement<AuthRemoteDatasource> $createElement(
+    $ProviderPointer pointer,
+  ) => $ProviderElement(pointer);
+
+  @override
+  AuthRemoteDatasource create(Ref ref) {
+    return authRemoteDatasource(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(AuthRemoteDatasource value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<AuthRemoteDatasource>(value),
+    );
+  }
+}
+
+String _$authRemoteDatasourceHash() =>
+    r'e3db12bb22c61875c8eef6e2272cdeca8cd6bb3e';
+
+@ProviderFor(gradeDatasource)
+final gradeDatasourceProvider = GradeDatasourceProvider._();
+
+final class GradeDatasourceProvider
+    extends
+        $FunctionalProvider<GradeDatasource, GradeDatasource, GradeDatasource>
+    with $Provider<GradeDatasource> {
+  GradeDatasourceProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'gradeDatasourceProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$gradeDatasourceHash();
+
+  @$internal
+  @override
+  $ProviderElement<GradeDatasource> $createElement($ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  GradeDatasource create(Ref ref) {
+    return gradeDatasource(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(GradeDatasource value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<GradeDatasource>(value),
+    );
+  }
+}
+
+String _$gradeDatasourceHash() => r'cd5b9d43d33ded95c8d92ff6dd891281faca832d';
+
+@ProviderFor(producaoDatasource)
+final producaoDatasourceProvider = ProducaoDatasourceProvider._();
+
+final class ProducaoDatasourceProvider
+    extends
+        $FunctionalProvider<
+          ProducaoDatasource,
+          ProducaoDatasource,
+          ProducaoDatasource
+        >
+    with $Provider<ProducaoDatasource> {
+  ProducaoDatasourceProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'producaoDatasourceProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$producaoDatasourceHash();
+
+  @$internal
+  @override
+  $ProviderElement<ProducaoDatasource> $createElement(
+    $ProviderPointer pointer,
+  ) => $ProviderElement(pointer);
+
+  @override
+  ProducaoDatasource create(Ref ref) {
+    return producaoDatasource(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(ProducaoDatasource value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<ProducaoDatasource>(value),
+    );
+  }
+}
+
+String _$producaoDatasourceHash() =>
+    r'e67a92cff69543db527f9592d51483df86a691cf';
+
+@ProviderFor(authRepository)
+final authRepositoryProvider = AuthRepositoryProvider._();
+
+final class AuthRepositoryProvider
+    extends $FunctionalProvider<AuthRepository, AuthRepository, AuthRepository>
+    with $Provider<AuthRepository> {
+  AuthRepositoryProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'authRepositoryProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$authRepositoryHash();
+
+  @$internal
+  @override
+  $ProviderElement<AuthRepository> $createElement($ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  AuthRepository create(Ref ref) {
+    return authRepository(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(AuthRepository value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<AuthRepository>(value),
+    );
+  }
+}
+
+String _$authRepositoryHash() => r'85782c17a7a45c26ba968dea12877e2577363791';
+
+@ProviderFor(gradeRepository)
+final gradeRepositoryProvider = GradeRepositoryProvider._();
+
+final class GradeRepositoryProvider
+    extends
+        $FunctionalProvider<GradeRepository, GradeRepository, GradeRepository>
+    with $Provider<GradeRepository> {
+  GradeRepositoryProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'gradeRepositoryProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$gradeRepositoryHash();
+
+  @$internal
+  @override
+  $ProviderElement<GradeRepository> $createElement($ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  GradeRepository create(Ref ref) {
+    return gradeRepository(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(GradeRepository value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<GradeRepository>(value),
+    );
+  }
+}
+
+String _$gradeRepositoryHash() => r'282a6c7e5cb6c8854f842be09aa0fe40ece169ff';
+
+@ProviderFor(producaoRepository)
+final producaoRepositoryProvider = ProducaoRepositoryProvider._();
+
+final class ProducaoRepositoryProvider
+    extends
+        $FunctionalProvider<
+          ProducaoRepository,
+          ProducaoRepository,
+          ProducaoRepository
+        >
+    with $Provider<ProducaoRepository> {
+  ProducaoRepositoryProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'producaoRepositoryProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$producaoRepositoryHash();
+
+  @$internal
+  @override
+  $ProviderElement<ProducaoRepository> $createElement(
+    $ProviderPointer pointer,
+  ) => $ProviderElement(pointer);
+
+  @override
+  ProducaoRepository create(Ref ref) {
+    return producaoRepository(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(ProducaoRepository value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<ProducaoRepository>(value),
+    );
+  }
+}
+
+String _$producaoRepositoryHash() =>
+    r'705f74728e570a602708d96505f5e3920a8a435c';
